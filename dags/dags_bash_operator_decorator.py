3637597ce06be155e2a95489a4463b948076f899
mungio <moko90222@gmail.com>
2024-06-14 19:36:41 +0900

    decorator_plus

--- dags/dags_bash_operator_decorator.py
+++ dags/dags_bash_operator_decorator.py
@@ -3,23 +3,22 @@ import datetime
 import pendulum
 from airflow.operators.bash import BashOperator
 
-@dag(
+@DAG(
     dag_id="dags_bash_operator_decorator",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
     catchup=True,
     tags=["homework"],
 )
-def bash_t1():
-    BashOperator(
-    task_id="bash_t1",
-    bash_command="echo whoami",
-)
+def my_dag():
+    DAG.bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
 
-def bash_t2():
-    BashOperator(
-    task_id="bash_t2",
-    bash_command="echo $HOSTNAME",
-)
+    DAG.bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
 
-bash_t1() >> bash_t2()
+    DAG.bash_t1 >> DAG.bash_t2
